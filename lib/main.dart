a97d08477953ea5be1752c0ad97be394d53f673b
Nachsyas <nchsys.arham@gmail.com>
2025-09-12 17:36:05 +0700

    First commit - initial Flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,280 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Alignment',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Program Pertamaku'),
+          backgroundColor: Colors.blue,
+          title: const Text('Alignment'),
         ),
-        body: const Center(
-          child: Text('Nachsyas Arham Mumtaz Nashohi', style: TextStyle(fontSize: 24)),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            
+          children: [
+            const Text('Malang', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)
+            ),
+            const SizedBox(height: 10),
+            const Text('25\u00B0C', style: TextStyle(fontSize: 80 , color: Colors.black54)
+            ),
+            const SizedBox(height: 40),
+            Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CuacaHari(
+                Hari: 'Minggu',
+                icon: Icons.wb_sunny,
+                Suhu: '20\u00B0C',
+              ),
+              const SizedBox(width: 40),
+              CuacaHari(
+              Hari: 'Senin',
+              icon: Icons.cloudy_snowing,
+              Suhu: '23\u00B0C',
+            ),
+            const SizedBox(width: 40),
+            CuacaHari(
+              Hari: 'Selasa',
+              icon: Icons.cloud,
+              Suhu: '22\u00B0C',
+            )
+            ],
+            ),
+            
+          ],
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          // textBaseline: TextBaseline.alphabetic,
+          //     children: [
+          //       Text(
+          //         'Suhu', style: TextStyle(fontSize: 30)),
+          //       Text(
+          //         '25\u00B0C', style: TextStyle(fontSize: 75)
+          //       ),
+          //       Icon(
+          //         Icons.sunny, size: 45, color: Colors.amber)
+                
+
+
+
+
+
+              //   child: Row(mainAxisSize: MainAxisSize.min,
+              // children: [
+              //   KotakBiruJempolKecil(),
+              //   SizedBox(height: 20),
+              //   KotakBiruJempol(),
+              //   SizedBox(height: 20),
+              //   KotakBiruJempolKecil(),
+                // child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                //               children: [
+                //                 KotakBiruJempolKecil(),
+                //                 SizedBox(height: 20),
+                //                 KotakBiruJempol(),
+                //                 SizedBox(height: 20),
+                //                 KotakBiruJempolKecil(),
+              //   child: Row(mainAxisSize: MainAxisSize.max,
+              // children: [
+              //   KotakBiruJempolKecil(),
+              //   SizedBox(height: 20),
+              //   KotakBiruJempol(),
+              //   SizedBox(height: 20),
+              //   KotakBiruJempolKecil(),
+              // Baris pertama
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     KotakWarna(
+              //       color: Colors.blue,
+              //       icon:Icons.favorite,
+              //       iconName: 'Likes',
+              //     ),
+              //     const SizedBox(width: 20),
+              //     KotakWarna(
+              //       color: Colors.green,
+              //       icon: Icons.favorite,
+              //       iconName: 'Likes',
+              //     ),
+              //   ],
+              // ),
+              // const SizedBox(height: 20),
+              // // Baris kedua
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     KotakWarna(
+              //       color: Colors.orange,
+              //       icon: Icons.favorite,
+              //       iconName: 'Likes',
+              //     ),
+              //     const SizedBox(width: 20),
+              //     KotakWarna(
+              //       color: Colors.purple,
+              //       icon : Icons.favorite,
+              //       iconName: 'Likes',
+              //     ),
+              //   ],
+              // ),
+              // ],
+          ),
         ),
       ),
     );
   }
+          // KotakBiruJempol(),
+          // SizedBox(width: 20),
+          // KotakBiruJempol(),
+          // SizedBox(width: 20),
+          // KotakBiruJempol(),
+          // SizedBox(height: 20),
+          // KotakBiruJempol(),
+          // SizedBox(height: 20),
+
+
+
+
+        // Row(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: const [
+        //   KotakBiruJempol(),
+        //   SizedBox(width: 20),
+        //   KotakBiruJempol(),
+        //   SizedBox(width: 20),
+        //   KotakBiruJempol(),  //
+        // child: KotakBiruJempol()
+      
+  
 }
 
+
+class CuacaHari extends StatelessWidget {
+  final String Hari;
+  final IconData icon;
+  final String Suhu;
+
+  const CuacaHari({super.key, required this.icon, this.Hari = '', this.Suhu = ''});
+
+  Widget build( BuildContext context) {
+    return Column(
+      children: [
+        Text(Hari, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        const SizedBox(height: 10),
+        Icon(icon, size: 45, color: Colors.amber),
+        const SizedBox(height: 10),
+        Text(
+          Suhu, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500))
+      ],
+    );
+  }
+
+}
+
+// class KotakBiruJempol extends StatelessWidget {
+//   const KotakBiruJempol({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 100,
+//       height: 100,
+//       decoration: BoxDecoration(
+//         color: Colors.blue,
+//         border: Border.all(color: Colors.black, width: 2),
+//         borderRadius: BorderRadius.circular(12),
+//       ),
+//       child: Icon(Icons.thumb_up, color: Colors.white, size: 40),
+//     );
+//   }
+// }
+
+// class KotakBiruJempolKecil extends StatelessWidget {
+//   const KotakBiruJempolKecil({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 75,
+//       height: 75,
+//       decoration: BoxDecoration(
+//         color: Colors.blue,
+//         border: Border.all(color: Colors.black, width: 2),
+//         borderRadius: BorderRadius.circular(12),
+//       ),
+//       child: Icon(Icons.thumb_up, color: Colors.white, size: 40),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+// class KotakWarna extends StatelessWidget {
+//   final Color color;
+//   final IconData  icon;
+//   final String iconName;
+
+//   const KotakWarna({super.key, required this.color, required this.icon, this.iconName = ''});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Container(
+//           width: 100,
+//           height: 100,
+//           decoration: BoxDecoration(
+//             color: color,
+//             border: Border.all(color: Colors.black, width: 2),
+//             borderRadius: BorderRadius.circular(12),
+//           ),
+//           child: const Icon(Icons.favorite, color: Colors.red, size: 40),
+//         ),
+//         const SizedBox(height: 8),
+//         Text(iconName, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+//         // const SizedBox(height: 8),
+//         // Icon(icon, size: 30, color: Colors.black),
+//       ],
+//     );
+//   }
+// }
+
+
+// class KotakBiruJempol extends StatelessWidget {
+//   const KotakBiruJempol({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 100,
+//       height: 100,
+//       decoration: BoxDecoration(
+//         color: Colors.blue,
+//         border: Border.all(color: Colors.black, width: 2),
+//         borderRadius: BorderRadius.circular(12),
+//       ),
+//       child: Icon(Icons.thumb_up, color: Colors.amber, size: 40),
+//     );
+//   }
+// }
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
