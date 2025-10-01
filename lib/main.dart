791c88e49968a72991b2d7def3fa24de432adf38
Nachsyas <nchsys.arham@gmail.com>
2025-10-01 14:22:39 +0700

    Update lib files untuk Modul 9

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,147 @@
-import 'home.dart';
-import 'tujuan.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+// Root App
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
-    );
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF0081c9),
-        ),
-        useMaterial3: true,
-      ),
-      initialRoute: '/', // halaman awal
-      routes: {
-        Tujuan.routeName: (context) => const Tujuan(),
-        '/': (context) => const Home(),
-      },
+      title: 'Stateful Widget Demo',
+      // Ganti home sesuai kebutuhan, bisa CounterPage atau LikeButtonPage
+      home: CounterPage(), 
+      // home: LikeButtonPage(),
     );
   }
 }
 
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
 
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
 
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0; // state
 
+  // Method untuk tambah
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
+  // Method untuk kurang
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter App')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Klik tombol tambah atau kurang:',
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              '$_counter',
+              style: const TextStyle(fontSize: 40),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue[700]!,
+                    minimumSize: const Size(100, 60),
+                  ),
+                  onPressed: _decrementCounter,
+                  child: const Text(
+                    'Kurang',
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue[700]!,
+                    minimumSize: const Size(100, 60),
+                  ),
+                  onPressed: _incrementCounter,
+                  child: const Text(
+                    'Tambah',
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
+class LikeButtonPage extends StatefulWidget {
+  const LikeButtonPage({super.key});
 
+  @override
+  State<LikeButtonPage> createState() => _LikeButtonPageState();
+}
 
+class _LikeButtonPageState extends State<LikeButtonPage> {
+  bool _isLiked = false; // state untuk status like
+  int _likeCount = 10;
 
+  // Method untuk toggle like
+  void _toggleLike() {
+    setState(() {
+      if (_isLiked) {
+        _likeCount--;
+        _isLiked = false;
+      } else {
+        _likeCount++;
+        _isLiked = true;
+      }
+    });
+  }
 
-// import 'package:flutter/material.dart';
-// import 'home.dart';
-// import 'tujuan.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       initialRoute: '/',
-//       routes: {
-//         '/': (context) => const Home(),
-//         Tujuan.routeName: (context) => const Tujuan(),
-//       },
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Like Button')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: Icon(
+                _isLiked ? Icons.favorite : Icons.favorite_border,
+                color: _isLiked ? Colors.red : Colors.grey,
+                size: 48,
+              ),
+              onPressed: _toggleLike,
+            ),
+            Text(
+              '$_likeCount likes',
+              style: const TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screen_arguments.dart
+++
@@ -1,9 +0,0 @@
-class ScreenArguments {
-  final String title;
-  final String cover;
-  final String description;
-  final String shortDesc;
-
-
-  ScreenArguments(this.title, this.cover, this.description, this.shortDesc);
-}
